1e84656824c05dbd6eb00cdcfd73bcbaf75a464b
FaXiR <fFaXiR@yandex.ru>
2020-02-22 15:20:45 +0500

    Создание класса UsingAcces

---
+++ Modules/UsingAccess.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursProject.Modules
+{
+    /// <summary>
+    /// Упрощенный доступ к БД Access
+    /// </summary>
+    class UsingAccess
+    {
+    }
+}
